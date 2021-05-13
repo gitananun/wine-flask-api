766606902bf09d837f4d91a9da1e85dc3b1806b0
Tigran <Tigran@Lionixs-MacBook-Pro.local>
2021-05-13 17:50:31 +0400

    output awkward values

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ import pandas as pd
 from flask import Flask, request, jsonify
 import pickle
 
-app = Flask('Wine')
-model = pickle.load(file := open('model.pkl', 'rb'))
+app = Flask("Wine")
+model = pickle.load(file := open("model.pkl", "rb"))
 
 
-@app.route('/', methods=['POST'])
+@app.route("/", methods=["POST"])
 def show():
     return jsonify({"data": request.json})
 
 
-@app.route('/predict', methods=['POST'])
+@app.route("/predict", methods=["POST"])
 def predict():
     data = request.json
 
@@ -21,7 +21,10 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return jsonify({"value": output, 'max': 10})
+    output = 10 if output > 10 else output
+    output = 0 if output < 0 else output
+
+    return jsonify({"value": output, "max": 10})
 
 
 file.close()
